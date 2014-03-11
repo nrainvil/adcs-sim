26ab3569cb0beebd8301148a56d08c093fcb6ceb
nira6794 <nira6794@vaio_rhel_6.(none)>
2014-03-10 21:57:28 -0600

    SVD Estimator

---
+++ Tools/AttitudeDet/est_svd.m
@@ -0,0 +1,17 @@
+function [ R ] = est_svd( b_k, eci_k )
+%EST_SVD 
+% input: b_k = 3xN input vector, body frame
+% input: eci_k = 3xN input vector, eci frame
+% output: R = rotation matrix, body to eci
+
+B = zeros(3,3);
+for k=1:length(b_k(1,:))
+    B = B + eci_k(:,k)*b_k(:,k)';
+end
+
+[U,S,V] = svd(B);
+M = diag([1,1,det(U)*det(V)]);
+R = U*M*V';
+
+end
+
